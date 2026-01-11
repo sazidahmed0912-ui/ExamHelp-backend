75c6443a9b754c3e9bd36a8e672334ebfeb28805
Sazid Ahmed <sazidahmed0912@gmail.com>
2026-01-11 11:07:45 +0530

    Fix PDF delivery system

--- routes/pdf.js
+++ routes/pdf.js
@@ -3,9 +3,15 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/:orderId", (req, res) => {
-  const filePath = path.join(__dirname, "../pdf/HSLC_ENGLISH_EBOOK.pdf");
+  const filePath = path.resolve(__dirname, "../pdfs/HSLC_ENGLISH_EBOOK.pdf");
+  console.log("📄 Serving PDF:", filePath);
 
-  res.sendFile(filePath);
+  res.sendFile(filePath, err => {
+    if (err) {
+      console.error("❌ PDF Send Error:", err);
+      res.status(404).send("PDF not found");
+    }
+  });
 });
 
 module.exports = router;
