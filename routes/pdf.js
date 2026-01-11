0804088649964c8c5728ab24866b900a6c678448
Sazid Ahmed <sazidahmed0912@gmail.com>
2026-01-11 10:44:24 +0530

    Fix PDF auto open stable

--- routes/pdf.js
+++ routes/pdf.js
@@ -3,13 +3,17 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/:orderId", (req, res) => {
-  const filePath = path.join(__dirname, "../protected/ebook.pdf");
+  const pdfPath = path.join(__dirname, "../pdf/HSLC_ENGLISH_EBOOK.pdf");
 
   res.setHeader("Content-Type", "application/pdf");
-  res.setHeader("Content-Disposition", "inline");
-  res.setHeader("Cache-Control", "no-store");
+  res.setHeader("Content-Disposition", "inline; filename=ebook.pdf");
 
-  res.sendFile(filePath);
+  res.sendFile(pdfPath, err => {
+    if (err) {
+      console.log("❌ PDF Error:", err);
+      res.status(404).send("PDF not found");
+    }
+  });
 });
 
 module.exports = router;
